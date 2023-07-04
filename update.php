26f27c5e446dede116d2b87b591c5b548c732730
Rakesh Kumar <cvceup@gmail.com>
2023-07-04 11:36:35 +0530

    inti

--- update.php
+++ update.php
@@ -6,3 +6,44 @@ if (isset($_POST['update_student'])) {
     $userController->updateUser($_POST, $id);
 }
 $data = $userController->getupdateUser($id);
+include('./thems/header.php');
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Update Student
+                        <a href="index.php" class="btn btn-danger float-end">Back</a>
+                    </h3>
+                </div>
+                <div class="card-body">
+
+                    <form method="post">
+                        <div class="mb-3">
+                            <label>Full Name</label>
+                            <input type="text" name="name" value="<?php echo $data['name'] ?>" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label>Email</label>
+                            <input type="text" name="email" value="<?php echo $data['email'] ?>" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label>Phone</label>
+                            <input type="text" name="phone" value="<?php echo $data['phone'] ?>" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label>class</label>
+                            <input type="text" name="course" value="<?php echo $data['course'] ?>" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" name="update_student" class="btn btn-primary">Update Student</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('./thems/footer.php');
